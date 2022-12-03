e9480e8777d0017cf7fd82f16896e0f0cce74a8e
cankskrk <ardacankisakurek@gmail.com>
2022-12-04 00:24:27 +0100

    Use Slugify and Update Routes

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
-router.route('/:id').get(courseController.getCourse);
+router.route('/:slug').get(courseController.getCourse);
 router.route('/').post(courseController.createCourse);
 
 module.exports = router;
